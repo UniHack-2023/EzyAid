3170353438c9a053eeb48bb06ee977e2884a7b44
Al3x-Myku <micu.georgealexandru@gmail.com>
2023-12-12 09:53:21 +0200

    database fix

--- #app.py
+++ #app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 import redis
 import json
+import requests
 
 app = Flask(__name__)
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
@@ -8,34 +9,49 @@ redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 @app.route('/', methods=['GET'])
 def show_inventory():
     items = redis_client.hgetall('items')
-    inventory = {item.decode('utf-8'): count.decode('utf-8') for item, count in items.items()}
-    return render_template('inventory.html', inventory=inventory)
+    inventory = []
 
-@app.route('/api/request/<item_name>', methods=['GET'])
-def request_item(item_name):
-    if redis_client.hexists('items', item_name):
-        return jsonify({'status': 200, 'message': f'{item_name} is available'})
-    else:
-        external_api_url = f'https://example.com/api/items/{item_name}'
-        response = requests.get(external_api_url)
+    for item, count in items.items():
+        item_name = item.decode('utf-8')
+        count = count.decode('utf-8')
 
-        if response.status_code == 200:
-            redis_client.hset('items', item_name, response.json()['availability'])
-            return jsonify({'status': 200, 'message': f'{item_name} is now available'})
+        # Retrieve characteristics from the Redis hash
+        characteristics_str = redis_client.hget(f'{item_name}_characteristics', 'characteristics')
+
+        if characteristics_str:
+            # Convert the JSON string back to a Python dictionary
+            characteristics = json.loads(characteristics_str.decode('utf-8'))
+            inventory.append({'item_name': item_name, **characteristics})
         else:
-            return jsonify({'status': 404, 'message': f'{item_name} is needed'})
+            inventory.append({'item_name': item_name, 'size': 0})  # Default to 0 if size not found
+
+    return render_template('inventory.html', inventory=inventory)
 
 @app.route('/api/add/<item_name>', methods=['POST'])
 def add_item(item_name):
-    count = redis_client.hincrby('items', item_name, 1)
+    existing_count = redis_client.hget('items', item_name)
+
+    if existing_count:
+        existing_count = int(existing_count.decode('utf-8'))
+        pairs_added = request.json.get('pairs', 0)
+        new_count = existing_count + pairs_added
+        redis_client.hset('items', item_name, new_count)
+    else:
+        new_count = 1
+        redis_client.hset('items', item_name, new_count)
 
     characteristics = request.json
 
+    # Extract 'size' from the characteristics and use it as the key
+    size = characteristics.get('size', 0)
+    characteristics[size] = characteristics.pop('size', 0)
+
     characteristics_str = json.dumps(characteristics)
 
-    redis_client.hset(f'{item_name}_characteristics', 'characteristics', characteristics_str)
+    redis_client.hset(f'{item_name}_characteristics', size, characteristics_str)
 
-    return jsonify({'status': 200, 'message': f'{item_name} added to the database ({count} times)'})
+    return jsonify({'status': 200, 'message': f'{item_name} added to the database ({new_count} times)'})
+    
 
 @app.route('/characteristics/<item_name>', methods=['GET'])
 def show_characteristics(item_name):
